174661ae93d47e8806b19d0ca1f4c27d268e758a
vikas <vikasyaligar.it@gmail.com>
2012-06-11 18:58:14 -0700

    hw3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+include MoviesHelper
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    store_url
     @all_ratings = ["G", "PG" , "PG-13" , "R"] 
     @select = []
     params["remember"] = []
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,17 @@
 module MoviesHelper
+
+  #Stores the address of ther given url
+  def store_url
+    session[:url] =  request.fullpath
+  end
   # Checks if a number is odd:
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  #Send to particular URl 
+  def send_user
+    session[:url]
+    session.delete(:url)
+  end
+  
 end
